aa90158b9c5bc6279a2447c7779378a13240b83e
kiarraafrica <158547111+kiarraafrica@users.noreply.github.com>
2024-12-18 10:33:52 +0800

    Add files via upload

---
+++ code/CrochetShop.java
@@ -0,0 +1,329 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class CrochetShop {
+    private Scanner input = new Scanner(System.in);
+    private User user = new User();
+    private ProductCatalog productCatalog = new ProductCatalog();
+    private ShoppingCart shoppingCart = new ShoppingCart();
+    private Wallet wallet = new Wallet();
+    
+    public void crochetShop(String username) {
+        boolean exit = false;
+
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/FinalProj_DBMS",
+                "root",
+                "MySQL0109030705"
+            );
+        }
+        catch (SQLException e) {
+            System.out.println("Error connecting to the database: " + e.getMessage());
+            return;
+        }
+
+        while (!exit) {
+            user.clearScreen();
+            System.out.println("+---------------------------------+");
+            System.out.println("          Crochet Shop Menu");
+            System.out.println("+---------------------------------+");
+            System.out.println("         1. Product Catalog");
+            System.out.println("          2. Shopping Cart");
+            System.out.println("             3. Wallet");
+            System.out.println("-----------------------------------");
+            System.out.println("           4. Log Out");
+            System.out.println("+---------------------------------+");
+            System.out.print("  > Enter your choice (1-4): ");
+            int choice = 0;
+
+            try {
+                choice = input.nextInt();
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Please enter a valid choice from the menu.");
+                input.next();
+                continue;
+            }
+
+            switch(choice) {
+                case 1:
+                    productCatalogMenu(connection);
+                    break;
+                case 2:
+                    shoppingCartMenu(connection, username);
+                    break;
+                case 3:
+                    walletMenu();
+                    break;
+                case 4:
+                    user.clearScreen();
+                    System.out.println("Logging out...");
+                    System.out.println();
+                    return;
+                default:
+                    System.out.println("Please enter a valid choice from the menu.");
+            }
+            input.nextLine();
+        }
+        
+        try {
+            connection.close();
+        }
+        catch (SQLException e) {
+            System.out.println("Error closing the database connection: " + e.getMessage());
+        }
+        input.close();
+    }
+
+    private void productCatalogMenu(Connection connection) {
+        boolean back = false;
+        
+        while (!back) {
+            user.clearScreen();
+            System.out.println("+---------------------------------+");
+            System.out.println("        Product Catalog Menu");
+            System.out.println("+---------------------------------+");
+            System.out.println("      1. View Crochet Tools");
+            System.out.println("          2. View Yarns");
+            System.out.println("     3. View Crocheted Items");
+            System.out.println("-----------------------------------");
+            System.out.println("            4. Exit");
+            System.out.println("+---------------------------------+");
+            System.out.print("  > Enter your choice (1-4): ");
+            int choice = 0;
+
+            try {
+                choice = input.nextInt();
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Please enter a valid choice from the menu.");
+                input.next();
+                continue;
+            }
+
+            switch (choice) {
+                case 1:
+                    productCatalog.viewCrochetTools(connection);
+                    break;
+                case 2:
+                    productCatalog.viewYarns(connection);
+                    break;
+                case 3:
+                    productCatalog.viewCrochetedItems(connection);
+                    break;
+                case 4:
+                    back = true;
+                    break;
+                default:
+                    System.out.println("Please enter a valid choice from the menu.");
+            }
+            input.nextLine();
+        }
+    }
+    
+    private void shoppingCartMenu(Connection connection, String username) {
+        boolean back = false;
+
+        while (!back) {
+            user.clearScreen();
+            System.out.println("+---------------------------------+");
+            System.out.println("         Shopping Cart Menu");
+            System.out.println("+---------------------------------+");
+            System.out.println("            1. View Cart");
+            System.out.println("           2. Add to Cart");
+            System.out.println("         3. Remove from Cart");
+            System.out.println("             4. Checkout");
+            System.out.println("-----------------------------------");
+            System.out.println("               5. Exit");
+            System.out.println("+---------------------------------+");
+            System.out.print("  > Enter your choice (1-5): ");
+            int choice = 0;
+
+            try {
+                choice = input.nextInt();
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Please enter a valid choice from the menu.");
+                input.next();
+                continue;
+            }
+
+            switch (choice) {
+                case 1:
+                    shoppingCart.displayCart();
+                    break;
+                case 2:
+                    addToCart(connection);
+                    break;
+                case 3:
+                    removeFromCart(connection);
+                    break;
+                case 4:
+                    checkoutCart(connection, username);
+                    break;
+                case 5:
+                    back = true;
+                    break;
+                default:
+                    System.out.println("Please enter a valid choice from the menu.");
+            }
+            input.nextLine();
+        }
+    }
+
+    private void addToCart(Connection connection) {
+        user.clearScreen();
+        System.out.println("+---------------------------------+");
+        System.out.println("             Add to Cart");
+        System.out.println("+---------------------------------+");
+        System.out.print(" > Enter product ID to add to cart: ");
+        String productID = input.nextLine();
+        System.out.print(" > Enter quantity: ");
+        int productQuantity = input.nextInt();
+
+        shoppingCart.addItem(connection, productID, productQuantity);
+    }
+
+    private void removeFromCart(Connection connection) {
+        user.clearScreen();
+        System.out.println("+---------------------------------+");
+        System.out.println("          Remove from Cart");
+        System.out.println("+---------------------------------+");
+        System.out.print(" > Enter product ID to remove from cart: ");
+        String productID = input.nextLine();
+
+        shoppingCart.removeItem(productID);
+    }
+
+    private void checkoutCart(Connection connection, String username) {
+        if (shoppingCart.cartItems.isEmpty()) {
+            System.out.println("Your cart is empty.");
+            return;
+        }
+
+        try {
+            String customerID = null;
+            String userQuery = "SELECT customer_id FROM users WHERE username = ?";
+            try (PreparedStatement userStmt = connection.prepareStatement(userQuery)) {
+                userStmt.setString(1, username);
+                ResultSet rs = userStmt.executeQuery();
+                if (rs.next()) {
+                    customerID = rs.getString("customer_id");
+                }
+            }
+
+            if (customerID == null) {
+                System.out.println("Unable to retrieve customer ID.");
+                return;
+            }
+
+            String query = "INSERT INTO orders (order_id, customer_id, username, product_id, name, price, quantity, total_price, date_time) VALUES (?, ?, ?, ?, ?, ?, ?, NOW())";
+            int orderID = generateOrderID(connection);
+
+            for (ShoppingCart.CartItem item : shoppingCart.cartItems) {
+                double totalPrice = item.getPrice() * item.getQuantity();
+
+                try (PreparedStatement stmt = connection.prepareStatement(query)) {
+                    stmt.setInt(1, orderID);
+                    stmt.setString(2, customerID);
+                    stmt.setString(3, username); 
+                    stmt.setString(4, item.getProductID());
+                    stmt.setString(5, item.getName());   
+                    stmt.setDouble(6, item.getPrice()); 
+                    stmt.setInt(7, item.getQuantity());
+                    stmt.setDouble(8, totalPrice);  
+                    stmt.executeUpdate();
+                }
+            }
+            System.out.println("Checkout successful! Your order has been placed.");
+            shoppingCart.cartItems.clear();
+        }
+        catch (SQLException e) {
+            System.out.println("Error saving checkout details: " + e.getMessage());
+        }
+    }
+
+    private int generateOrderID(Connection connection) {
+        try (Statement stmt = connection.createStatement()) {
+            String query = "SELECT MAX(order_id) FROM orders";
+            ResultSet rs = stmt.executeQuery(query);
+            if (rs.next()) {
+                return rs.getInt(1) + 1;
+            }
+        }
+        catch (SQLException e) {
+            System.out.println("Error generating order ID: " + e.getMessage());
+        }
+        return 1;
+    }
+
+    private void walletMenu() {
+        boolean back = false;
+
+        while (!back) {
+            user.clearScreen();
+            System.out.println("+---------------------------------+");
+            System.out.println("               Wallet");
+            System.out.println("+---------------------------------+");
+            System.out.println("          1. View Balance");
+            System.out.println("'        2. Deposit Money");
+            System.out.println("            3. Withdraw");
+            System.out.println("-----------------------------------");
+            System.out.println("              4. Exit");
+            System.out.println("+---------------------------------+");
+            System.out.print(" > Enter your choice (1-4): ");
+            int choice = 0;
+
+            try {
+                choice = input.nextInt();
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Please enter a valid choice from the menu.");
+                input.next();
+                continue;
+            }
+
+            switch (choice) {
+                case 1:
+                    System.out.printf("Current Balance: %.2f%n", wallet.getBalance());
+                    System.out.println("---------------------------------");
+                    System.out.println();
+                    System.out.print(" > Press 'enter' to return to shop menu.");
+                    input.nextLine();
+                    break;
+                case 2:
+                    user.clearScreen();
+                    System.out.println("+---------------------------------+");
+                    System.out.println("             DEPOSIT");              
+                    System.out.println("+---------------------------------+");     
+                    System.out.print(" > Enter amount to deposit: ");
+                    double depositAmount = input.nextDouble();
+                    wallet.deposit(depositAmount);
+                    break;
+                case 3:
+                    user.clearScreen();
+                    System.out.println("+---------------------------------+");
+                    System.out.println("             WITHDRAW");
+                    System.out.println("+---------------------------------+");
+                    System.out.print(" > Enter amount to withdraw: ");
+                    double withdrawAmount = input.nextDouble();
+                    wallet.withdraw(withdrawAmount);
+                    break;
+                case 4:
+                    back = true;
+                    break;
+                default:
+                    System.out.println("Please enter a valid choice from the menu.");
+            }
+            input.nextLine();
+        }
+    }
+} 
---
+++ code/MainMenu.java
@@ -0,0 +1,79 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MainMenu {
+    private Scanner input = new Scanner(System.in);
+    private User user = new User();
+    private CrochetShop crochetShop = new CrochetShop();
+    private AdminMenu adminMenu = new AdminMenu();
+
+    public void mainMenu() {
+        boolean exit = false;
+
+        while (!exit) {
+            System.out.println("+----------------------------------+");
+            System.out.println("              Main Menu");
+            System.out.println("+----------------------------------+");
+            System.out.println("            1. Sign Up");
+            System.out.println("             2. Log In");
+            System.out.println("          3. Admin Log In");
+            System.out.println("-----------------------------------");
+            System.out.println("              4. Exit");
+            System.out.println("+----------------------------------+");
+            System.out.print("  > Enter your choice (1-4): ");
+            int choice = 0;
+
+            try {
+                choice = input.nextInt();
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Please enter a valid choice from the menu.");
+                input.next();
+                continue;
+            }
+
+            switch(choice) {
+                case 1:
+                    user.signUp();
+                    break;
+                case 2:
+                    if (user.logIn()) {
+                        crochetShop.crochetShop(user.getCurrentUsername());
+                    }
+                    break;
+                case 3: 
+                    adminLogin();
+                    break;
+                case 4:
+                    System.out.println("Exiting...");
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Please enter a valid choice from the menu.");
+            }
+            input.nextLine();
+        }
+        input.close();
+    }
+
+    private void adminLogin() {
+        String adminUsername = "admin";
+        String adminPassword = "admin123";
+
+        System.out.println("+----------------------------------+");
+        System.out.println("             ADMIN LOGIN");
+        System.out.println("+----------------------------------+");
+        System.out.print(" > Enter admin username: ");
+        String username = input.next();
+        System.out.print(" > Enter admin password: ");
+        String password = input.next();
+
+        if (username.equals(adminUsername) && password.equals(adminPassword)) {
+            System.out.println("Admin login successful!");
+            adminMenu.adminMenu();
+        }
+        else {
+            System.out.println("Inavlid admin credentials.");
+        }
+    }
+}
